1519cfbe0005a0d106835702d581b01d9429f894
Temiloluwa <temilolu74@yahoo.co.uk>
2020-11-18 17:22:31 +0100

    included model and web

---
+++ web/src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LanguageModel from './components/LanguageModel';
+
+ReactDOM.render(<LanguageModel/>, document.getElementById('app'))
