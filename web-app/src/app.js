05089d977102c5a87fb3855778714fbc4d161b2e
Temiloluwa <temilolu74@yahoo.co.uk>
2020-11-18 15:57:29 +0100

    removed webapp

--- web-app/src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LanguageModel from './components/LanguageModel';
-
-ReactDOM.render(<LanguageModel/>, document.getElementById('app'))
