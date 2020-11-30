6daccfe86a68b7261c1aaefb0ab58d380099e299
Temiloluwa <temilolu74@yahoo.co.uk>
2020-11-30 21:33:54 +0100

    node_proxy_working

--- web/server/server.js
+++ web/server/server.js
@@ -5,6 +5,16 @@ const publicPath = path.join(__dirname, '..', 'public')
 const port = 3000;
 
 app.use(express.static(publicPath));
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+//const handler = (req, res, next) => {
+//    next();
+//};
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
